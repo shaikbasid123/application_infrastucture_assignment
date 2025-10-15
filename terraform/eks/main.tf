7b6a8ce0545bedfd0af2ccf0b9f99f87241716c3
shaikbasid123 <109240630+shaikbasid123@users.noreply.github.com>
2025-10-15 23:26:31 +0530

    Add EKS cluster and node group resources

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -1,1 +1,14 @@
+resource "aws_eks_cluster" "main" {
+  name     = "my-eks-cluster"
+  role_arn = aws_iam_role.eks_role.arn  # Assume role is created
+  vpc_config {
+    subnet_ids = var.private_subnets
+  }
+}
 
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "eks-node-group"
+  subnet_ids      = var.private_subnets
+  # Add more configurations as needed
+}
